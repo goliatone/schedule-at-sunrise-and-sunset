9dd3ee7f9aedd6d434a1555ec4571c1a0807ad2f
goliatone <burgosemi@gmail.com>
2015-12-18 14:18:18 -0500

    clean up

--- conf/schedule.js
+++ conf/schedule.js
@@ -3,11 +3,11 @@
  */
 var join = require('path').join,
     resolve = require('path').resolve;
-// var humanToCron = require('human-to-cron');
+
 module.exports =
 {
     id: 'sunset-sunrise-test',
     command: join(process.cwd(), 'bin', 'at-suncalc-date'),
-    when: '58 23 * * *', //humanToCron('23:58:00')
+    when: '58 23 * * *', //23:58:00
     comment: 'Scheduler for sunrise/sunset actions.'
 };
